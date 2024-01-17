044bf8866fe9cf2f489baaa1232b6e081aa679fc
gabribn <mnfgabriel@gmail.com>
2024-01-17 18:45:26 -0300

    User insertion method POST

--- src/main/java/com/gabrielreis/apirest/services/UserService.java
+++ src/main/java/com/gabrielreis/apirest/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielreis.apirest.domain.User;
+import com.gabrielreis.apirest.dto.UserDTO;
 import com.gabrielreis.apirest.repository.UserRepository;
 import com.gabrielreis.apirest.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
