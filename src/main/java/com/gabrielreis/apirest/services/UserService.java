91985a554ff34158419b3ac594fa809f1200469d
gabribn <mnfgabriel@gmail.com>
2024-01-17 18:16:16 -0300

    Getting a User by Id and Not found exception

--- src/main/java/com/gabrielreis/apirest/services/UserService.java
+++ src/main/java/com/gabrielreis/apirest/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gabrielreis.apirest.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielreis.apirest.domain.User;
 import com.gabrielreis.apirest.repository.UserRepository;
+import com.gabrielreis.apirest.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
