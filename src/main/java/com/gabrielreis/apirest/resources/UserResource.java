09a9f651f5779db114df4e53cf1e01f5e4fea283
gabribn <mnfgabriel@gmail.com>
2024-01-18 14:15:02 -0300

    EndPoint to return User posts and Getting a post by Id

--- src/main/java/com/gabrielreis/apirest/resources/UserResource.java
+++ src/main/java/com/gabrielreis/apirest/resources/UserResource.java
@@ -6,9 +6,17 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gabrielreis.apirest.domain.Post;
 import com.gabrielreis.apirest.domain.User;
 import com.gabrielreis.apirest.dto.UserDTO;
 import com.gabrielreis.apirest.services.UserService;
@@ -53,4 +61,12 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@GetMapping("/{id}/posts")
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+	
 }
