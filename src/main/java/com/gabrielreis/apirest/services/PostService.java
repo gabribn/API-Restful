ec72e3eebf99f8d72f082a554b34372d9b238af6
gabribn <mnfgabriel@gmail.com>
2024-01-18 16:20:20 -0300

    Query methods

--- src/main/java/com/gabrielreis/apirest/services/PostService.java
+++ src/main/java/com/gabrielreis/apirest/services/PostService.java
@@ -1,5 +1,7 @@
 package com.gabrielreis.apirest.services;
 
+import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,4 +21,12 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
