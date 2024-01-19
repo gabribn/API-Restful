9f1b53ed65b20b35989afb4f017aa7bd1e703b29
gabribn <mnfgabriel@gmail.com>
2024-01-19 11:20:38 -0300

    Added CrossOrigin

--- src/main/java/com/gabrielreis/apirest/resources/PostResource.java
+++ src/main/java/com/gabrielreis/apirest/resources/PostResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.gabrielreis.apirest.resources.util.URL;
 import com.gabrielreis.apirest.services.PostService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping(value="/posts")
 public class PostResource {
 	
